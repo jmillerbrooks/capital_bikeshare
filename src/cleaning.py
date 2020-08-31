7f55db5b81d0daa32272ca5ad3679c907dab37cc
Jake <brooksjacobm@gmail.com>
2020-08-31 13:04:10 -0400

    cleaning.py will successfully clean raw_apr_to_jul ETL runs cleaning.py and loads resulting longform data into post gis, ebikes.py holds outdated versions of the cleaning functions

--- src/cleaning.py
+++ src/cleaning.py
@@ -9,6 +9,48 @@ gdf = anc_gdf()
 anc_dict = station_anc_dict()
 station_keys = anc_dict.keys()
 
+##Ugly dude. Ugly. Split this up.
+def clean_frame(df):
+    """Perform a number of cleaning operations on the dockless ebikes data. Return clean df.
+    
+    Params: (df) a pandas DataFrame of trip data on dockless ebikes from Capital Bikeshare
+    
+    Returns: (df) a cleaned pandas DataFrame of trip data on dockless ebikes from Capital Bikeshare"""
+    
+    
+    # Remove trips that have missing lat/lng coords
+    df = df.loc[~(df.start_lat.isna() | df.start_lng.isna())]
+    df = df.loc[~(df.end_lat.isna()| df.end_lng.isna())]
+    
+    # Variable to hold the location of the warehouse, these trips should not be included in analysis
+    warehouse = '6035 Warehouse'
+    
+    # Remove trips that end at the warehouse
+    df = df.loc[df.end_station_name != warehouse]
+    
+    # Convert all datetimes to datetime format
+    df['started_at'] = pd.to_datetime(df.started_at)
+    df['ended_at'] = pd.to_datetime(df.ended_at)
+    
+    # Add duration_seconds column
+    df['duration'] = df.ended_at - df.started_at
+    df['duration_seconds'] = df.duration.dt.total_seconds()
+
+    cleaned_df = df.drop(['duration'], axis=1)
+    
+
+    # Remove trips that last longer than 15 hours (outliers)
+    cleaned_df = cleaned_df.loc[cleaned_df.duration_seconds < 60*60*15]
+    # Remove trips with negative duration
+    cleaned_df = cleaned_df.loc[cleaned_df.duration_seconds > 0]
+
+    # Remove start_station_id and end_station_id, this information is duplicated in station name cols, as well as lng/lat combination
+    cleaned_df = cleaned_df.drop(['start_station_id', 'end_station_id'], axis=1)
+    
+    
+    # Return the cleaned df
+    return cleaned_df
+
 def point_series(lng_col, lat_col, name, index_col):
     """Takes 2 pandas series (don't pass a df) and returns a GeoSeries of shapely POINT objects, indexed by index_col"""
     # Zip lng/lat together, and make a point out of each 
@@ -32,10 +74,10 @@ def apply_anc(row, side='start'):
     """DOCSTRING"""
     if side == 'start':
         station = row.start_station_name
-        coord = row.start_coord
+        coord = row['coord-start']
     else:
         station = row.end_station_name
-        coord = row.end_coord
+        coord = row['coord-end']
     
     lookup = anc_from_dict(station)
     if lookup:
@@ -53,6 +95,22 @@ def anc_cols(df):
     # Create 'ANC_end' column with the 
     df['ANC-end'] = df.apply(lambda x: apply_anc(x, side='end'), axis=1)
     return df
+
+
+def rename_columns(df, legacy=False):
+    """DOCSTRING
+    build out legacy=True to rename the columns of the old version of the data
+    Prepares columns for the wide-to-long function
+    """
+    df = df.rename(
+        columns={
+            'started_at':'time-start',
+            'ended_at':'time-end',
+            'start_station_name':'station_name-start',
+            'end_station_name':'station_name-end'
+        })
+    return df
+
 
 def to_geo(df, ANC=True):
     """Custom function to transform cleaned_ebikes into GeoDataFrame"""
@@ -87,101 +145,8 @@ def to_geo(df, ANC=True):
     
     return geo_ebikes
 
-def rename_columns(df, legacy=False):
-    """DOCSTRING
-    build out legacy=True to rename the columns of the old version of the data
-    """
-    df = df.rename(
-        columns={
-            'started_at':'time-start',
-            'ended_at':'time-end',
-            'start_station_name':'station_name-start',
-            'end_station_name':'station_name-end'
-        })
-    return df
 
-##Ugly dude. Ugly. Split this up.
-def clean_frame(df):
-    """Perform a number of cleaning operations on the dockless ebikes data. Return clean df.
-    
-    Params: (df) a pandas DataFrame of trip data on dockless ebikes from Capital Bikeshare
-    
-    Returns: (df) a cleaned pandas DataFrame of trip data on dockless ebikes from Capital Bikeshare"""
-    
-    
-    # Remove trips that have missing lat/lng coords
-    df = df.loc[~(df.start_lat.isna() | df.start_lng.isna())]
-    df = df.loc[~(df.end_lat.isna()| df.end_lng.isna())]
-    
-    # Variable to hold the location of the warehouse, these trips should not be included in analysis
-    warehouse = '6035 Warehouse'
-    
-    # Remove trips that end at the warehouse
-    df = df.loc[df.end_station_name != warehouse]
-    
-    df = to_geo(df)
-    
-    
-    
-    # Convert all datetimes to datetime format
-    df['started_at'] = pd.to_datetime(df.started_at)
-    df['ended_at'] = pd.to_datetime(df.ended_at)
-    
-    # Engineer some additional features such as start/end hour of day, day of week and duration
-#     df['started_hour'] = df.started_at.dt.hour
-#     df['ended_hour'] = df.ended_at.dt.hour
-#     df['day_of_week'] = df.started_at.dt.day_name()
-    df['duration'] = df.ended_at - df.started_at
-    df['duration_seconds'] = df.duration.dt.total_seconds()
-
-
-    
-    # Drop duration, and ride_id columns.
-    # We have duration seconds, ride_id is not particularly useful 
-    # since we are interested in aggregate numbers instead of individual trips
-    # DEBUGGING INCLUDING (, 'ride_id')
-    cleaned_df = df.drop(['duration'], axis=1)
-    
-    
-
-    
-    # Remove trips that last longer than 15 hours (outliers)
-    cleaned_df = cleaned_df.loc[cleaned_df.duration_seconds < 60*60*15]
-    # Remove trips with negative duration
-    cleaned_df = cleaned_df.loc[cleaned_df.duration_seconds > 0]
-
-    # Remove start_station_id and end_station_id, this information is duplicated in station name cols, as well as lng/lat combination
-    cleaned_df = cleaned_df.drop(['start_station_id', 'end_station_id'], axis=1)
-    
-    cleaned_df = rename_cols(cleaned_df)
-    
-    # Return the cleaned df
-    return cleaned_df
-
-
-
-def load_clean_full():
-    """DOCSTRING MAKE THIS EXTENSIBLE TO MORE MONTHS"""
-    df = pd.read_pickle('../data/wip/raw_apr_to_jul_df.pkl')
-    cleaned_full = clean_frame(df)
-    return cleaned_full
-
-def rename_cols(df):
-    """Prepare Column Names for wide_to_long operation below"""
-    renamed = df.rename(
-        columns={'started_at':'time-start',
-                 'ended_at':'time-end',
-                 'ANC_start':'ANC-start',
-                 'ANC_end':'ANC-end',
-                 'geom_start':'coord-start',
-                 'geom_end':'coord-end',
-                 'start_station_name':'station_name-start',
-                 'end_station_name':'station_name-end'
-                })
-    return renamed
-
-
-def long_ts_geo(df):
+def to_long(df):
     """DOCSTRING making the long family more general
     returns long format version of geodataframe indexed
     by time"""
@@ -193,7 +158,7 @@ def long_ts_geo(df):
     # (so do all other columns not prefixed with an
     # element appearing in stubnames)
     rides_long = pd.wide_to_long(
-        full_import,
+        df,
         stubnames=['time', 'ANC', 'station_name', 'coord'],
         i='ride_id',
         j='start_end',
@@ -207,7 +172,19 @@ def long_ts_geo(df):
         .sort_index()
     
     # Return GeoDataFrame version
-    return gpd.GeoDataFrame(rides_long_ts)
+    return gpd.GeoDataFrame(rides_long_ts, geometry='coord')
+
+def full_transform(df):
+    """DOCSTRING
+    Accepts Raw Data from (FOR RIGHT NOW JUST APR to JUL)
+    Returns Long Format Data to Load into POSTGIS db"""
+    transformed = clean_frame(df)
+    transformed = to_geo(transformed)
+    transformed = rename_columns(transformed)
+    long = to_long(transformed)
+    return long
+    
+    
 
 def net_gain_loss(location, df, col='ANC'):
     """Return an np.array of the effect of ride on a given column value's net gain or loss in 1 0 -1 VALUES
@@ -266,112 +243,6 @@ def cumulative_change(df, window_size):
     
 
     
-### DEPRECATE THESE? MOVE TO A DIFFERENT MODULE?
-
-## NEEDS WORK!! FIX GET_DATA MODULE SO THAT LOAD CLEAN DOCKLESS CAN JUST CALL FROM THERE
-def load_clean_dockless():
-    # FIX THIS CALL GET_DATA MODULE
-    df = pd.read_pickle('../data/wip/raw_dockless.pkl')
-    cleaned_ebikes = clean_frame(df)
-    cleaned_ebikes = cleaned_ebikes.drop('rideable_type', axis=1)
-    return cleaned_ebikes
-
-    
-    
-def load_geo_ebikes():
-    df = load_clean_dockless()
-    geo_ebikes = to_geo(df)
-    return geo_ebikes
-
-def geo_longer(df):
-    """NEEDS DOCSTRING THIS FUNCTION MAKES ONE TIME COLUMN FROM START/END
-    AND DOUBLES THE LENGTH OF THE DF IN PROCESS, A GOOD TEST IS WHETHER OR NOT
-    THE LEN IS 2x OG DF"""
-    # List all the columns that are not start/end time for easy melt operation below
-    cols = list(df.columns)
-    cols.remove('started_at')
-    cols.remove('ended_at')
-
-    # Combine started_at/ended_at into one column 'time', indicating whether
-    # this was a trip start or trip end in another column, 'start_end', 
-    # set index of new df to 'time'
-    # sort the index, so it makes sense as a time series
-    long_geo = df.rename(columns={'started_at': 'start', 'ended_at': 'end'}) \
-        .melt(id_vars=cols \
-              , value_vars=['start', 'end'] \
-              , var_name='start_end' \
-              , value_name='time') \
-        .set_index('time') \
-        .sort_index()
-    return long_geo
-
-def load_long_geo():
-    """DOCSTRING"""
-    df = load_geo_ebikes()
-    long_geo = geo_longer(df)
-    return long_geo
-
-def load_long_geo_full():
-    """DOCSTRING"""
-    df = load_geo_ebikes()
-    long_geo = geo_longer(df)
-    return long_geo
-
-def anc_frame(df):
-    """DOCSTRING"""
-    
-    anc_df = df.drop(['start_station_name', 'end_station_name'], axis=1)
-    return anc_df
-
-def load_long_anc():
-    """DOCSTRING"""
-    df = load_long_geo()
-    anc_df = anc_frame(df)
-    return anc_df
-
-
-# NEEDS WORK!! FIX DOCSTRING!! GENERALIZE TO ANY LOCATION COL (station etc.) 
-# This is likely uneccesary now that we have a more generalized long df function
-def net_gain_loss_anc(ANC_name, df):
-    """NEEDS DOCSTRING THIS FUNCTION RETURNS A SERIES (list? np.array?) OF 1 0 -1 VALUES
-    1 if RIDE ENDED IN ANC 0 IF RIDE DID NOT LEAVE OR END IN ANC -1 IF RIDE LEFT FROM ANC"""
-    
-    conditions = [
-        (df['start_end'] == 'start') & (df['ANC_start'] == ANC_name),
-        (df['start_end'] == 'end') & (df['ANC_end'] == ANC_name),
-        (df['ANC_start'] != ANC_name) & (df['ANC_end'] != ANC_name),
-        (df['start_end'] == 'end') & (df['ANC_end'] != ANC_name),
-        (df['start_end'] == 'start') & (df['ANC_start'] != ANC_name)
-        ]
-
-    values = [
-        -1,
-        1,
-        0,
-        0,
-        0
-    ]
-    
-    return np.select(conditions, values)
-
-def plus_minus_anc_frame(df):
-    """DOCSTRING GENERALIZE THIS FUNCTION TO ACCEPT OTHER THINGS BESIDE ANC REMOVE DEPENDENCY ON GDF"""
-    
-    
-    # Create dictionary of ancs (keys) and series of plus minus values returned from net_gain_loss_anc (values)
-    # for each unique ANC_ID
-    plus_minus_dict = {anc: net_gain_loss_anc(anc, df) \
-                       for anc in \
-                       list(gdf.ANC_ID)}
-    # Convert dict to dataframe, index by the (time) index of long_anc_df passed
-    anc_plus_minus_df = pd.DataFrame(plus_minus_dict, index=df.index)
-    
-    return anc_plus_minus_df
-
-def load_plus_minus_anc():
-    df = load_long_anc()
-    plus_minus = plus_minus_anc_frame(df)
-    return plus_minus
 
 
 
---
+++ src/ebikes.py
@@ -0,0 +1,128 @@
+### DEPRECATE THESE? OLD VERSIONS OF CLEANING FUNCTIONS FOR JUST EBIKES
+### NO LONGER WORKING WITH THESE
+
+
+import pandas as pd
+import numpy as np
+from shapely.geometry import Point
+import geopandas as gpd
+from src.utils import which_anc, station_anc_dict
+from src.get_data import anc_gdf
+
+gdf = anc_gdf()
+anc_dict = station_anc_dict()
+station_keys = anc_dict.keys()
+
+
+
+
+## NEEDS WORK!! FIX GET_DATA MODULE SO THAT LOAD CLEAN DOCKLESS CAN JUST CALL FROM THERE
+def load_clean_dockless():
+    # FIX THIS CALL GET_DATA MODULE
+    df = pd.read_pickle('../data/wip/raw_dockless.pkl')
+    cleaned_ebikes = clean_frame(df)
+    cleaned_ebikes = cleaned_ebikes.drop('rideable_type', axis=1)
+    return cleaned_ebikes
+
+    
+    
+def load_geo_ebikes():
+    df = load_clean_dockless()
+    geo_ebikes = to_geo(df)
+    return geo_ebikes
+
+def load_clean_full():
+    """DOCSTRING MAKE THIS EXTENSIBLE TO MORE MONTHS"""
+    df = pd.read_pickle('../data/wip/raw_apr_to_jul_df.pkl')
+    cleaned_full = clean_frame(df)
+    return cleaned_full
+
+def geo_longer(df):
+    """NEEDS DOCSTRING THIS FUNCTION MAKES ONE TIME COLUMN FROM START/END
+    AND DOUBLES THE LENGTH OF THE DF IN PROCESS, A GOOD TEST IS WHETHER OR NOT
+    THE LEN IS 2x OG DF"""
+    # List all the columns that are not start/end time for easy melt operation below
+    cols = list(df.columns)
+    cols.remove('started_at')
+    cols.remove('ended_at')
+
+    # Combine started_at/ended_at into one column 'time', indicating whether
+    # this was a trip start or trip end in another column, 'start_end', 
+    # set index of new df to 'time'
+    # sort the index, so it makes sense as a time series
+    long_geo = df.rename(columns={'started_at': 'start', 'ended_at': 'end'}) \
+        .melt(id_vars=cols \
+              , value_vars=['start', 'end'] \
+              , var_name='start_end' \
+              , value_name='time') \
+        .set_index('time') \
+        .sort_index()
+    return long_geo
+
+def load_long_geo():
+    """DOCSTRING"""
+    df = load_geo_ebikes()
+    long_geo = geo_longer(df)
+    return long_geo
+
+def load_long_geo_full():
+    """DOCSTRING"""
+    df = load_geo_ebikes()
+    long_geo = geo_longer(df)
+    return long_geo
+
+def anc_frame(df):
+    """DOCSTRING"""
+    
+    anc_df = df.drop(['start_station_name', 'end_station_name'], axis=1)
+    return anc_df
+
+def load_long_anc():
+    """DOCSTRING"""
+    df = load_long_geo()
+    anc_df = anc_frame(df)
+    return anc_df
+
+
+# NEEDS WORK!! FIX DOCSTRING!! GENERALIZE TO ANY LOCATION COL (station etc.) 
+# This is likely uneccesary now that we have a more generalized long df function
+def net_gain_loss_anc(ANC_name, df):
+    """NEEDS DOCSTRING THIS FUNCTION RETURNS A SERIES (list? np.array?) OF 1 0 -1 VALUES
+    1 if RIDE ENDED IN ANC 0 IF RIDE DID NOT LEAVE OR END IN ANC -1 IF RIDE LEFT FROM ANC"""
+    
+    conditions = [
+        (df['start_end'] == 'start') & (df['ANC_start'] == ANC_name),
+        (df['start_end'] == 'end') & (df['ANC_end'] == ANC_name),
+        (df['ANC_start'] != ANC_name) & (df['ANC_end'] != ANC_name),
+        (df['start_end'] == 'end') & (df['ANC_end'] != ANC_name),
+        (df['start_end'] == 'start') & (df['ANC_start'] != ANC_name)
+        ]
+
+    values = [
+        -1,
+        1,
+        0,
+        0,
+        0
+    ]
+    
+    return np.select(conditions, values)
+
+def plus_minus_anc_frame(df):
+    """DOCSTRING GENERALIZE THIS FUNCTION TO ACCEPT OTHER THINGS BESIDE ANC REMOVE DEPENDENCY ON GDF"""
+    
+    
+    # Create dictionary of ancs (keys) and series of plus minus values returned from net_gain_loss_anc (values)
+    # for each unique ANC_ID
+    plus_minus_dict = {anc: net_gain_loss_anc(anc, df) \
+                       for anc in \
+                       list(gdf.ANC_ID)}
+    # Convert dict to dataframe, index by the (time) index of long_anc_df passed
+    anc_plus_minus_df = pd.DataFrame(plus_minus_dict, index=df.index)
+    
+    return anc_plus_minus_df
+
+def load_plus_minus_anc():
+    df = load_long_anc()
+    plus_minus = plus_minus_anc_frame(df)
+    return plus_minus
